05be37b6766eae1cf90695d537f056f789d1e0ac
Andrii.Kovalchuk <shtramak@gmail.com>
2022-09-29 20:53:02 +0300

    add person-api and jfairy library

--- openapi/openapi-server/src/main/java/com/bobocode/petros/openapiserver/OpenapiServerApplication.java
+++ openapi/openapi-server/src/main/java/com/bobocode/petros/openapiserver/OpenapiServerApplication.java
@@ -1,13 +1,26 @@
 package com.bobocode.petros.openapiserver;
 
+import com.devskiller.jfairy.Fairy;
+import com.devskiller.jfairy.producer.RandomGenerator;
+import com.devskiller.jfairy.producer.person.DefaultPersonProvider;
+import com.devskiller.jfairy.producer.person.Person;
+import com.devskiller.jfairy.producer.person.PersonFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class OpenapiServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OpenapiServerApplication.class, args);
+//        SpringApplication.run(OpenapiServerApplication.class, args);
+        Fairy fairy = Fairy.create();
+        var people = IntStream.rangeClosed(0, 10)
+                .boxed()
+                .map(i -> fairy.person())
+                .toList();
     }
 
 }
